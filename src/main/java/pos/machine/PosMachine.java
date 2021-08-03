537f4d942572f22fd11c8fca422b208ee6f115fc
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-03 23:58:00 +0800

    [ST-002][ADRIAPA]feat: Convert barcodes to items with details

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,32 @@
 package pos.machine;
-
-import java.util.List;
+import java.util.*;
 
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
-        return null;
+
+//    public static void main(String[] args) {
+//        PosMachine posMachine = new PosMachine();
+//        System.out.println(posMachine.printReceipt(ItemDataLoader.loadBarcodes()));
+//    }
+
+    private List<Item> convertToItems(List<String> barcodes) {
+        LinkedList<Item> itemsWithDetail = new LinkedList<Item>();
+        List<ItemInfo> itemsInfo = loadAllItemsInfo();
+        barcodes = new ArrayList<>(new LinkedHashSet<>(barcodes));
+        for (String barcode : barcodes) {
+            Item itemDetail = new Item();
+            for (ItemInfo itemInfoVal : itemsInfo) {
+                if (barcode.equals(itemInfoVal.getBarcode())) {
+                    itemDetail.setName(itemInfoVal.getName());
+                    itemDetail.setUnitPrice(itemInfoVal.getPrice());
+                    itemDetail.setQuantity(Collections.frequency(ItemDataLoader.loadBarcodes(), barcode));
+                }
+            }
+            itemsWithDetail.add(itemDetail);
+        }
+        return itemsWithDetail;
+    }
+
+    private List<ItemInfo> loadAllItemsInfo(){
+        return ItemDataLoader.loadAllItemInfos();
     }
 }
